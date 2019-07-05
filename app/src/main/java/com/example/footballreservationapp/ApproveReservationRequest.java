477c2e8f2161ec043b870bde024bef1d98610801
Jiho-korea <ghok1027@naver.com>
2019-07-05 22:52:52 +0900

    관리자 페이지 에서의 예약 취소기능, 예약 승인 기능 추가

--- app/src/main/java/com/example/footballreservationapp/ApproveReservationRequest.java
+++ app/src/main/java/com/example/footballreservationapp/ApproveReservationRequest.java
@@ -5,7 +5,7 @@ import com.android.volley.toolbox.StringRequest;
 
 import java.util.HashMap;
 import java.util.Map;
-
+//예약 승인해주는 
 public class ApproveReservationRequest  extends StringRequest {
     final static private String URL = "http://yonamfootball.dothome.co.kr/ApproveReservation.php";
     private Map<String, String> parameters;
