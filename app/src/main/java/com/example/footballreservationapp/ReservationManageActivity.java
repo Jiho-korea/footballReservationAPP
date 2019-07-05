addf38b4917420d3ffa68614d34613bb4cc10b5a
Jiho-korea <ghok1027@naver.com>
2019-07-05 16:49:27 +0900

    예약취소 기능 추가

---
+++ app/src/main/java/com/example/footballreservationapp/ReservationManageActivity.java
@@ -0,0 +1,13 @@
+package com.example.footballreservationapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationManageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_manage);
+    }
+}
